0a70409bd11e19a9892dead4ab1cb8f2cf0d4c47
anuzza <anuja3580@gmail.com>
2022-06-29 06:03:57 -0500

    worked with multiple states

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,23 @@
-import {Component} from 'react';
+import {useState, Component} from 'react';
 import {useSelector, useDispatch, connect} from 'react-redux';
 import classes from './Counter.module.css';
 
 
+
 const Counter = () => {
+
+  
   const dispatch = useDispatch();
 
+
   const counter = useSelector(state =>state.counter);
-  const toggleCounterHandler = () => {};
+
+  const show = useSelector(state => state.showCounter)
+
+
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'})
+  };
 
   const incrementHandler=()=>{
     dispatch({type: 'increment'});
@@ -27,7 +37,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
