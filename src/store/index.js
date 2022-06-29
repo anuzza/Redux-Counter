e28b3dce3d53b04d4480de43be107d696c3ca483
anuzza <anuja3580@gmail.com>
2022-06-29 05:15:05 -0500

    created a redux store

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import {createStore} from 'redux';
+
+
+const counterReducer=(state={counter :0}, action)=>{
+    if(action.type === "increment"){
+        return {
+            counter : state+1
+
+        }
+    }
+
+    if(action.type === 'decrement'){
+        return {
+            counter : state -1
+        }
+    }
+
+    return state;
+
+};
+
+const store = createStore(counterReducer);
+
+export default store;
+
+
+
+
+
+
+
+
+
